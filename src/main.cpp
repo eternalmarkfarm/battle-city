aaba04ab9dd43831eb213c9c6573524bf2cbd8cd
eternalmarkfarm <nksepte@gmail.com>
2024-04-08 23:17:51 +0500

    Added GLAD Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,41 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
-    GLFWwindow* window;
+    GLFWwindow *window;
 
-if (!glfwInit())
-    return -1;
+    if (!glfwInit())
+        return -1;
 
-// Устанавливаем параметры GLFW для прозрачности окна (если поддерживается)
-glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+    // Устанавливаем параметры GLFW для прозрачности окна (если поддерживается)
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
 
-GLFWwindow* window = glfwCreateWindow(640, 480, "Transparent Window", NULL, NULL);
-if (!window)
-{
-    glfwTerminate();
-    return -1;
-}
+    window = glfwCreateWindow(640, 480, "Transparent Window", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL())
+    {
+        std::cout << "Cant load GLAD" << std::endl;
+        return -1;
+    }
+
+    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+
+    glClearColor(1, 1, 0 , 1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -35,4 +47,3 @@ glfwMakeContextCurrent(window);
     glfwTerminate();
     return 0;
 }
-
