59c81863c8ce5c799ad785f75bfb0c3bffbebf6a
eternalmarkfarm <nksepte@gmail.com>
2024-04-08 22:43:19 +0500

    Added glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main(){
- 
-	std::cout<<"Hello World!"<<std::endl;
+int main(void)
+{
+    GLFWwindow* window;
 
-	return 0;
+if (!glfwInit())
+    return -1;
+
+// Устанавливаем параметры GLFW для прозрачности окна (если поддерживается)
+glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+GLFWwindow* window = glfwCreateWindow(640, 480, "Transparent Window", NULL, NULL);
+if (!window)
+{
+    glfwTerminate();
+    return -1;
+}
+
+glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
 
