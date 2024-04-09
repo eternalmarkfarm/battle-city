4bbbf4fccf48106e81b217c0e33facffbae90501
eternalmarkfarm <nksepte@gmail.com>
2024-04-09 20:43:49 +0500

    Added some function on code

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,47 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int heigth){
+    g_windowSizeX = width;
+    g_windowSizeY = heigth;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode){
+    if(key==GLFW_KEY_ESCAPE && action==GLFW_PRESS){
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+}
+
 int main(void)
 {
-    GLFWwindow *window;
-
+    
     if (!glfwInit())
-        return -1;
-
-    // Устанавливаем параметры GLFW для прозрачности окна (если поддерживается)
-    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
-
-    window = glfwCreateWindow(640, 480, "Transparent Window", NULL, NULL);
-    if (!window)
     {
+        std::cout<<"glfwInit failed" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    
+    GLFWwindow *pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", nullptr, nullptr);
+    if (!pWindow)
+    {
+        std::cout<<"glfwCreateWindow failed"<<std::endl;
         glfwTerminate();
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
+    glfwSetKeyCallback(pWindow,glfwKeyCallback);
+
+    glfwMakeContextCurrent(pWindow);
 
     if (!gladLoadGL())
     {
@@ -27,18 +50,20 @@ int main(void)
         return -1;
     }
 
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION)<<std::endl;
 
-    glClearColor(1, 1, 0 , 1);
+
+    glClearColor(1, 1, 0, 1);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
